ba000e208a6303810ca1a3860a57025eea88fdcd
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-04-09 13:30:14 +0200

    Création du bundle BadgeBundle + création des entités Badge & BadgeUnlock

---
+++ src/Amacabr2/BadgeBundle/BadgeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Amacabr2\BadgeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BadgeBundle extends Bundle
+{
+}
---
+++ src/Amacabr2/BadgeBundle/Entity/BadgeUnlock.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Amacabr2\BadgeBundle\Entity;
+
+use AppBundle\AppBundle;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * BadgeUnlock
+ *
+ * @ORM\Table(name="badge_unlock")
+ * @ORM\Entity(repositoryClass="Amacabr2\BadgeBundle\Repository\BadgeUnlockRepository")
+ */
+class BadgeUnlock
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var Badge
+     *
+     * @ORM\ManyToOne(targetEntity="Amacabr2\BadgeBundle\Entity\Badge", inversedBy="unlocks")
+     */
+    private $badge;
+
+    /**
+     * @var \AppBundle\Entity\User
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Badge")
+     */
+    private $user;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set badge
+     *
+     * @param \Amacabr2\BadgeBundle\Entity\Badge $badge
+     *
+     * @return BadgeUnlock
+     */
+    public function setBadge(\Amacabr2\BadgeBundle\Entity\Badge $badge = null)
+    {
+        $this->badge = $badge;
+
+        return $this;
+    }
+
+    /**
+     * Get badge
+     *
+     * @return \Amacabr2\BadgeBundle\Entity\Badge
+     */
+    public function getBadge()
+    {
+        return $this->badge;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \AppBundle\Entity\Badge $user
+     *
+     * @return BadgeUnlock
+     */
+    public function setUser(\AppBundle\Entity\Badge $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \AppBundle\Entity\Badge
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
